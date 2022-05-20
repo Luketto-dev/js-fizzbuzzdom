2f9ba97a47bd03aab7815d7260c39fbf7f23c2b6
Luketto-dev <luca.francesconi1995@gmail.com>
2022-05-20 16:47:29 +0200

    cambiato stile agli square a seconda del valore dell indice

--- js/script.js
+++ js/script.js
@@ -4,38 +4,38 @@ console.dir(divSquareContainer)
 
 
 
-
 // faccio un ciclo per stampare tutti i nueri da 1 a 100
 for (let i = 1; i <= 100; i++) {
     // creo variabile testo 
     let testo = ""
 
+    let color = ""
 
-    // se i è divisibile per 3 e per 5 cambio la variabile testo e la stampo in console
+    // se i è divisibile per 3 e per 5 cambio la variabile testo 
     if(i % 3 == 0 && i % 5 == 0){
         testo = "FizzBuzz";
-        
+        color = "bg-red"
     }
-    // se i è divisibile per 3 cambio la variabile testo e la stampo in console
+    // se i è divisibile per 3 cambio la variabile testo 
     else if (i % 3 == 0) {
         testo = "fizz";
-        
+        color = "bg-green"
     }
-    // se i è divisibile per 5 cambio la variabile testo e la stampo in console
+    // se i è divisibile per 5 cambio la variabile testo
     else if (i % 5 == 0){
         testo = "buzz";  
-        
+        color = "bg-yellow"
     }
-    // in tutti gli altri casi stampo la i in console
+    // in tutti gli altri casi la variabile testo è uguale ad i 
     else{
         testo = i.toString()
-        
+        color = "bg-blue"
     }
 
     // stampo l avariabile testo ad ogni iterazione : la variabile cambia ogni volta volta che si entra in un if diverso 
     console.log(testo)
 
-    divSquareContainer.innerHTML += `<div class="square">
+    divSquareContainer.innerHTML += `<div class="square ${color}">
                                         ${testo}
                                     </div>`    
 
